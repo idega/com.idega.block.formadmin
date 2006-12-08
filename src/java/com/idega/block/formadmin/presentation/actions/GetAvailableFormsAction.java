46078eb43cca6199e735a27887b29234b48d7c11
Gediminas Paulauskas <gediminas@idega.com>
2006-12-08 15:37:37 +0000

    create FormsService, move all WebDAV-related code there

--- src/java/com/idega/block/formadmin/presentation/actions/GetAvailableFormsAction.java
+++ src/java/com/idega/block/formadmin/presentation/actions/GetAvailableFormsAction.java
@@ -1,33 +1,43 @@
 package com.idega.block.formadmin.presentation.actions;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+import java.util.logging.Logger;
 import javax.faces.event.ActionEvent;
 import javax.faces.event.ActionListener;
-
-import com.idega.block.form.business.AvailableFormBean;
-import com.idega.block.form.business.AvailableFormsLister;
+import javax.faces.model.SelectItem;
+import com.idega.block.form.business.FormsService;
 import com.idega.block.formadmin.presentation.components.PhaseManagedGridHtmlDataTable;
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.idegaweb.IWMainApplication;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas �ivilis</a>
  * @version 1.0
  */
 public class GetAvailableFormsAction implements ActionListener, IPhaseValueProvider {
+	private static final Logger log = Logger.getLogger(GetAvailableFormsAction.class.getName()); 
 	
 	public void processAction(ActionEvent ae) {
 		
 	}
 	
-	public static final String COLUMNID_FORM_NAME = "formTitle";
+	public static final String COLUMNID_FORM_NAME = "label";
 	
-	public List getAvailableForms() {
-	
-		List<AvailableFormBean> available_forms = AvailableFormsLister.getInstance().getAvailableForms();
+	public List getAvailableForms() {	
+		List<SelectItem> forms = new ArrayList<SelectItem>();
+		try {
+			forms = getFormsService().listForms();
+		}
+		catch (RemoteException e) {
+			log.warning("Error getting available forms");
+		}
 		
-		return available_forms;
+		return forms;
 	}
 	
 	public static void initiateTableColumnsProperties(Map session_map) {
@@ -47,4 +57,17 @@ public class GetAvailableFormsAction implements ActionListener, IPhaseValueProvi
 		
 		return "Get available forms";
 	}
+	
+	private FormsService getFormsService() {
+		FormsService service = null;
+		try {
+			IWApplicationContext iwc = IWMainApplication.getDefaultIWApplicationContext();
+			service = (FormsService) IBOLookup.getServiceInstance(iwc, FormsService.class);
+		}
+		catch (IBOLookupException e) {
+			log.severe("Could not find FormsService");
+		}
+		return service;
+	}
+	
 }
